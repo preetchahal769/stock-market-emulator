1cffcbaa89253aa6bf15a24d6d26f8f00998cb07
Cyberrayon04846 <cyberrayon@gmail.com>
2024-12-02 10:35:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 //  Libary Imports
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 
 // import routes
 import authRoutes from "./routes/auth.routes.js";
@@ -12,4 +13,5 @@ const PORT = process.env.PORT || 5000;
 app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
 });
