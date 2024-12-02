86c6dc50b89271944599754aa98d5a1fbf308c99
Cyberrayon04846 <cyberrayon@gmail.com>
2024-12-02 19:29:17 +0530

    Login and Logout funcnality completed

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 // import routes
 import authRoutes from "./routes/auth.routes.js";
@@ -10,6 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
