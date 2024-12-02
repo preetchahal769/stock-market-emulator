f7c56e10e9e32f583e115a77e99007ed5a7aedbb
Cyberrayon04846 <cyberrayon@gmail.com>
2024-12-02 10:08:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,5 +7,5 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost: ${PORT}`);
 });
