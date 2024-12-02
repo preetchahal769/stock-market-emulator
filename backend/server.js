9ed1b2ca0c4f1a36ff89f8e1c3e45454a98630af
Cyberrayon04846 <cyberrayon@gmail.com>
2024-12-02 10:20:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,14 @@
 import express from "express";
 import dotenv from "dotenv";
 
+// import routes
+import authRoutes from "./routes/auth.routes.js";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost: ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
