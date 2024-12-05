79953c3eaaa79d63dac2e64fa00a274bb6004d6f
Cyberrayon04846 <cyberrayon@gmail.com>
2024-12-05 21:37:37 +0530

    Frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./Pages/Home";
+import LoginPage from "./Pages/LoginPage";
+import SignupPage from "./Pages/SignupPage";
 
 function App() {
   return (
-    <>
-      {" "}
-      <p className=" text-3xl text-red-500">Hello world!</p>
-    </>
+    <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
+      <div className="absolute inset-0 overflow-hidden">
+        <div class="w-full h-full absolute top-0 bg-gradient-to-br from-gray-100 to-gray-200 shadow-md  p-10"></div>
+      </div>
+      <div className="relative z-10 pt-20">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
---
+++ frontend/src/Pages/SignupPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignupPage = () => {
+  return <div class="bg-white rounded-lg shadow-md p-10 max-lg:3rem"></div>;
+};
+
+export default SignupPage;
