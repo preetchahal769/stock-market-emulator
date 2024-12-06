963becaa184b5a84d485d86d10ae7d57ee443d3b
Cyberrayon04846 <cyberrayon@gmail.com>
2024-12-06 10:47:22 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
       <div className="absolute inset-0 overflow-hidden">
-        <div class="w-full h-full absolute top-0 bg-gradient-to-br from-gray-100 to-gray-200 shadow-md  p-10"></div>
+        <div class="w-full h-full absolute top-0 bg-gradient-to-br from-orange-200 to-green-200 shadow-md  p-10"></div>
       </div>
       <div className="relative z-10 pt-20">
         <Routes>
